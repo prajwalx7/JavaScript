8b285b8622dff30cf84112928053769edccf6670
Prajwal <prajwal77d@gmail.com>
2025-01-16 19:02:51 +0530

    memory

--- Basics/datatypes_summary.js
+++ Basics/datatypes_summary.js
@@ -33,16 +33,52 @@ let myData = {
     name: "Prajwal",
     age: 22,
 }
-
 // console.log(myData);
-
 
 
 // Functions
 
 const myFunc = function(){
-    console.log("Hello World");
+    // console.log("Hello World");
 }
 
+// myFunc();
 
-myFunc();
+function addition(a,b){
+    sum = a + b;
+    return sum;
+}
+
+// console.log(addition(2,3));
+
+// **** Stack and Heap memory ****
+
+
+// Stack Memory stores primitive values
+// Last in first out strucuture
+// faster data access
+// Memory is automatically allocated when a function is called and deallocated when the function execution is complete.
+
+let myName = "Prajwal";
+let newName = myName;
+anotherName = "Praj";
+console.log(newName);
+console.log(anotherName);
+
+
+// Heap Memory stores objects
+// unstructured data
+// slower data access
+// Memory is automatically allocated when an object is created and deallocated when the object is no longer needed.
+// JavaScript uses a garbage collector to free up unused memory in the heap.
+
+let userOne = {
+    email : "tester@gmail.com",
+    name : "Prajwal"
+}
+
+let userTwo = userOne;
+userTwo.name = "john";
+
+console.log(userOne.name);
+console.log(userTwo.name);
